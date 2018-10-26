644c2d78ec0aaf1c28385355cbb7f103e89cf14d
kris675s <kris675s@eul.eal.dk>
2018-10-26 13:57:44 +0200

    sdf

---
+++ TournamentTest/ControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TournamentTest
+{
+    [TestClass]
+    public class ControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
